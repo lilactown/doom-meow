c364229d4433e38698d92fcdb82ea21a413615ce
Not Leader <notleader3@gmail.com>
2022-02-11 16:12:25 +0300

    change package pin

--- packages.el
+++ packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/meow/packages.el
 
-(package! meow :pin "371f6554acfa4ecb9fc121f23539f77064f97115")
+(package! meow :pin "6bb3fee0b91a74622e891537307918e828a4ed89")
